3fe0def96223c15bb06b8e3480f9d8a9cc4e2d89
Jason Tian <kuohuitien@gmail.com>
2023-07-24 14:57:28 +0800

    feat: automatic assign available drive letter (#2)

--- MAIN/mount.bat
+++ MAIN/mount.bat
@@ -6,6 +6,8 @@ FOR %%A IN ("%~dp0.") DO SET parentDir=%%~dpA
 SET mode=%1
 SET config=%2.ini
 
+SET letters=A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+
 IF "%mode%"=="read" (
   SET mode=ro
 )
@@ -13,9 +15,20 @@ IF "%mode%"=="write" (
   SET mode=ts
 )
 
+for %%l in (%letters%) do (
+  echo checking %%l...
+  wmic logicaldisk where "name='%%l:'" get name | findstr /i "%%l" > nul
+  if errorlevel 1 (
+    SET availDrive=%%l
+    goto break
+  )
+)
+:break
+
+echo Available Drive Letter is: %availDrive%
+
 for /f "tokens=1,2 delims==" %%a in (%parentDir%data\%config%) do (
 if %%a==program set program=%%b
-if %%a==drive set drive=%%b
 if %%a==volume set volume=%%b
 if %%a==password set password=%%b
 )
@@ -31,7 +44,7 @@ IF DEFINED program (
 )
 
 IF DEFINED password (
-%program% /l %drive% /m %mode% /v "%parentDir%data\%volume%" /a /p %password%
+%program% /l %availDrive% /m %mode% /v "%parentDir%data\%volume%" /a /p %password%
 ) ELSE (
-%program% /l %drive% /m %mode% /v "%parentDir%data\%volume%" /a
+%program% /l %availDrive% /m %mode% /v "%parentDir%data\%volume%" /a
 )
