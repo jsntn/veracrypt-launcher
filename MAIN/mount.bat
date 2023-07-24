da0210a890f6b72f3e8eebef0962f02f0e0956b6
Jason Tian <kuohuitien@gmail.com>
2023-07-24 17:07:59 +0800

    refactor: remove the volume setting from .ini file

--- MAIN/mount.bat
+++ MAIN/mount.bat
@@ -5,6 +5,7 @@ FOR %%A IN ("%~dp0.") DO SET parentDir=%%~dpA
 
 SET mode=%1
 SET config=%2.ini
+SET volume=%2
 
 SET letters=A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
 
@@ -18,7 +19,6 @@ IF "%mode%"=="write" (
 for /f "tokens=1,2 delims==" %%a in (%parentDir%data\%config%) do (
 if %%a==program set program=%%b
 if %%a==drive set drive=%%b
-if %%a==volume set volume=%%b
 if %%a==password set password=%%b
 )
 
